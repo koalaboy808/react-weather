6870db5158689aaac7ef27d9ee4c3c14f57f1ab1
koalaboy808 <carlo_liquido@ischool.berkeley.edu>
2018-09-26 13:18:47 -0700

    adding chart and weather list

---
+++ src/containers/weather_list.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart';
+
+class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(weather => weather.main.pressure);
+    const humidity = cityData.list.map(weather => weather.main.humidity);
+
+    return (
+      <tr key = {name}>
+        <td> {name} </td>
+        <td>
+          <Chart data={temps} color="red" units="degrees"/>
+        </td>
+        <td>
+          <Chart data={pressure} color="blue" units="hpa" />
+        </td>
+        <td>
+          <Chart data={humidity} color="green" units="%" />
+        </td>
+      </tr>
+    )
+  }
+
+  render() {
+    return (
+      <table className="table table-hover">
+        <thead>
+          <tr>
+            <th> City </th>
+            <th> Temp </th>
+            <th> Pressure </th>
+            <th> Humid </th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.weather.map(this.renderWeather)}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({weather}) {
+  // if the above were... (state) instead of ({weather})
+  // return { weather: state.weather};
+  return {weather} // same as --> { weather : weather}
+}
+
+export default connect(mapStateToProps)(WeatherList)
--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,4 +1,22 @@
-export default function (state = null, action) {
+import { FETCH_WEATHER } from '../actions/index';
+
+// set inital state to [] instead of null since we will insert into an array
+export default function (state = [], action) {
   console.log('Action received ', action);
+  console.log('state ', state);
+
+  switch (action.type) {
+    case FETCH_WEATHER:
+      // WRONG - below piece of code manipulates state directly
+      // similar to react, in redux you should never mutate state directly
+      // such as through this.state. = xyz
+      // return state.push(action.payload.data)
+
+      // CORRECT since it creates an entirely new stance of state
+      // return state.concat([action.payload.data]);
+
+      // ALSO CORRECT
+      return [action.payload.data, ...state];
+  }
   return state;
 }
